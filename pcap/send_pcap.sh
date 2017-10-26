873910eafdd25dca41056ebb6f8742d2a02438f5
mackhendricks <ec2-user@demo.hortonworks.com>
2017-10-26 21:09:36 +0000

    Added logic to send pcap info

--- pcap/send_pcap.sh
+++ pcap/send_pcap.sh
@@ -1,16 +1,19 @@
+#!/bin/bash
+set -x
 START=$1
 END=$2
 EMAIL=$3
 UUID=`uuidgen`
 PCAP_WEBROOT=/tmp/www/pcap
 PCAP_HTTP_PORT=80
+PCAP_FULLPATH=$PCAP_WEBROOT/$UUID
 #
-mkdir -p $PCAP_WEBROOT/$UUID
-cd $PCAP_WEBROOT/$UUID
-/usr/hcp/current/metron/bin/pcap_query.sh fixed -st $START -et $END 
+mkdir -p $PCAP_FULLPATH
+cd $PCAP_FULLPATH
+/usr/hcp/current/metron/bin/pcap_query.sh fixed -st $START #-et $END 
 if [ $? -eq 0 ];then
     HOSTNAME=`curl -s ip.alt.io`
     echo "A PCAP file was generated.  Please check your email to obtain a link for accessing the PCAP(s)."
-    echo "A PCAP file was genearated and can be found at http://$HOSTNAME:$PCAP_HTTP_PORT/pcap/$UUID" | mail -s "PCAP file for $START to $END" $EMAIL
+    echo "A PCAP file was genearated and can be found at http://$HOSTNAME:$PCAP_HTTP_PORT/$UUID" | mail -s "PCAP file for $START" $EMAIL
     
 fi
